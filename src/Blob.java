9259892702a907d977db2051dc9f21dd81143b17
Christian Gutowski <cgutowski>
2017-04-01 21:38:09 -0400

    Filled in code for blob from https://github.com/homosapien97/MapGame/blob/master/MapGame/src/utility/FractalBlob.java

--- src/Blob.java
+++ src/Blob.java
@@ -5,21 +5,63 @@
 import javafx.geometry.Point2D;
 
 import java.util.LinkedList;
-import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 
 public class Blob extends LinkedList<Point2D> {
     private static final int DEFAULT_DEFORMATIONS = 20;
     private static final double DEFAULT_SCALE = 1024.0;
     private static final RegularPolygon TRIANGLE = new RegularPolygon(new Point2D(0,0), 3, DEFAULT_SCALE);
+    private Random rand;
 
-    public Blob() {
+    public Blob(int seed, double jaggedness) {
         super(TRIANGLE);
+        rand = new Random(seed);
+        LinkedList<Point2D> newPoints = new LinkedList<>();
+        ListIterator<Point2D> iter;
+        Point2D a;
+        Point2D b;
         for(int i = 0; i < DEFAULT_DEFORMATIONS; i++) {
-            deform();
+            for(iter = this.listIterator(); iter.hasNext();) {
+                a = iter.next();
+                if(iter.hasNext()) {
+                    b = iter.next();
+                } else {
+                    b = this.getFirst();
+                }
+                iter.previous();
+                iter.add(getMutation(a, b, jaggedness / (i + 1)));
+            }
         }
     }
 
-    private static void deform() {
+    private Point2D getMutation(Point2D a, Point2D b, double jaggedness) {
+        double rx;
+        double ry;
+        double dx = b.getX() - a.getX();
+        double dy = b.getY() - a.getY();
+        if(dx == 0) {
+            rx = 0;
+            ry = dy * centerRand();
+        } else {
+            rx = dx * centerRand();
+            ry = rx * dy / dx;
+        }
+        if(dy == 0) {
+            dy = dx * (rand.nextDouble() - 0.5) * jaggedness;
+            dx = 0;
+        } else {
+            double minv = -dx/dy;
+            dx = Math.sqrt((dx * dx + dy * dy) / (minv * minv + 1) * (rand.nextDouble() - 0.5) * jaggedness);
+            dy = minv * dx;
+        }
+        rx += dx + a.getX();
+        ry += dy + a.getY();
+        return new Point2D(rx, ry);
+    }
 
+    double centerRand() {
+        double ret = rand.nextDouble();
+        return ret * (4 * ret * ret - 6 * ret + 3);
     }
 }
